e4a45d1cced9a7a8f6a58dd4cef874557c788a8e
Daniel Evans <dev@microsoft.com>
2018-12-21 10:36:32 -0800

    Cleanup

--- Http2AspCoreExperiment1/Controllers/TunnelController.cs
+++ Http2AspCoreExperiment1/Controllers/TunnelController.cs
@@ -1,13 +1,5 @@
-﻿using System.Collections.Generic;
-using System.IO.Pipelines;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Connections;
+﻿using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http2.FlowControl;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
 
 namespace Http2AspCoreExperiment1.Controllers
 {
@@ -15,19 +7,22 @@ namespace Http2AspCoreExperiment1.Controllers
     [ApiController]
     public class TunnelController : ControllerBase
     {
+        #region controllers
         // GET: api/Chat
         [HttpGet]
         public HttpResponseMessage Get()
         {
             return PassItAlong();
         }
+        #endregion
 
-        public static HttpResponseMessage PassItAlong()
-        {
+        #region private
+        private static HttpResponseMessage PassItAlong()
+        {          
             //await may not play well with 500s, so making it a bit dumber for now
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new System.Uri("Read the endpoint from appsettings here.");
+                client.BaseAddress = new System.Uri("https://http2aspcoreexperiment120181219030212.azurewebsites.net");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept
                     .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
@@ -38,35 +33,6 @@ namespace Http2AspCoreExperiment1.Controllers
                 return response.Result;
             }
         }
-
-        //private void DoSomething(Http2Stream http2Stream)
-        //{
-        //    Task.Factory.StartNew(async () =>
-        //    {
-        //        try
-        //        {
-        //            var context = new Http2StreamContext();
-
-        //        }
-        //        catch { };
-        //    }
-
-        //    );
-
-        //    Http2StreamContext http2StreamContext = new Http2StreamContext();
-        //    Http2Stream bob = new Http2Stream(http2StreamContext);
-        //    Http2FrameWriter writer =
-        //        new Http2FrameWriter(
-        //            new PipeWriter,
-        //            new ConnectionContext,
-        //            new Http2Connection,
-        //            new OutputFlowControl,
-        //            new ITimeoutControl,
-        //            new MinDataRate,
-        //            this.HttpContext.Connection.Id,
-        //            this.Request.HttpContext.,
-        //            new IKestrelTrace
-        //        )
-        //}
+        #endregion
     }
 }
